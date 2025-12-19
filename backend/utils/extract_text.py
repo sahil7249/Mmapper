ac215fb65d41babcbd9c144602727750a500e593
sahil7249 <pathansahil7249@gmail.com>
2025-12-19 12:04:00 +0530

    init utils and add extract_text function

---
+++ backend/utils/extract_text.py
@@ -0,0 +1,14 @@
+import sys
+import pymupdf
+
+def extract_text_from_pdf(pdf_path):
+    doc = pymupdf.open(pdf_path)
+    text = ""
+    for page in doc:
+        text += page.get_text()
+    return text
+
+if __name__ == "__main__":
+    pdf_path = sys.argv[1]
+    extracted_text = extract_text_from_pdf(pdf_path)
+    return extracted_text
