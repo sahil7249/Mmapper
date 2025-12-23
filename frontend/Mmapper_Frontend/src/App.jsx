f414a9d2e946506b5a291852746af62505a2cf2f
sahil7249 <pathansahil7249@gmail.com>
2025-12-23 16:13:19 +0530

    add footer

--- frontend/Mmapper_Frontend/src/App.jsx
+++ frontend/Mmapper_Frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./page-components/Navbar"
 import { Main } from "./page-components/Main"
+import { Footer } from "./page-components/Footer"
 const App = () => {
   return (
     <>
       <Navbar />
       <Main />
+      <Footer />
     </>
   )
 }
---
+++ frontend/Mmapper_Frontend/src/page-components/Footer.jsx
@@ -0,0 +1,9 @@
+import logo from '../assets/gitlogo.png'
+
+export const Footer = () => {
+    return(
+        <div className="fixed bottom-0 text-center w-screen border-t h-10 pt-1 text-xl">
+            ©2025 Made with ♥️ in INDIA
+        </div>
+    )
+}
