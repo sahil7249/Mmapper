d1232e4188ce587457bea8a6c3720d141a9cd486
sahil7249 <pathansahil7249@gmail.com>
2025-12-25 19:01:36 +0530

    add api fetch

--- frontend/Mmapper_Frontend/src/App.jsx
+++ frontend/Mmapper_Frontend/src/App.jsx
@@ -3,18 +3,19 @@ import { Main } from "./page-components/Main"
 import { Footer } from "./page-components/Footer"
 import { MindMapContainer } from "./page-components/MindMapContainer"
 import { MapList } from "./page-components/MapList"
-import { createContext, useState } from "react"
+import { createContext,useState } from "react"
 
 export const UIStateContext = createContext(null)
 const App = () => {
   const [state,setState] = useState("default")
+  const [data,setData] = useState("")
 
   return (
     <>
-    <UIStateContext.Provider value={{state:state,setState:setState}}>
+    <UIStateContext.Provider value={{state:state,setState:setState,data:data,setData:setData}}>
       <Navbar />
       {state == "default" && <Main />}
-      {state == "mindmap" && <MindMapContainer />}
+      {state == "mindmap" && <MindMapContainer markdown={data} />}
       {state == "list" && <MapList maps={[]}/>}
       <Footer />
     </UIStateContext.Provider>
--- frontend/Mmapper_Frontend/src/page-components/Main.jsx
+++ frontend/Mmapper_Frontend/src/page-components/Main.jsx
@@ -1,4 +1,36 @@
+import { UIStateContext } from "../App"
+import { useContext } from "react"
+
 export const Main = () => {
+
+    const { setState, setData } = useContext(UIStateContext)
+
+    const handleFileUpload = async (e) => {
+        const file = e.target.files[0]
+        const formdata = new FormData()
+        formdata.append('pdf', file)
+
+        try {
+            const response = await fetch('http://localhost:8080/api/process-pdf', {
+                method: 'POST',
+                body: formdata
+            })
+
+            if (!response.ok) {
+                console.error('Error: ', response.error)
+            }
+
+            const data = await response.json()
+
+            const markdown = data.markdown
+            setData(markdown)
+            setState('mindmap')
+        } catch (error) {
+            console.log('Error while uploading a file: ', error)
+        }
+    }
+
+
     return (
         <div className="mt-10">
             <div className="w-screen text-center">
@@ -14,7 +46,7 @@ export const Main = () => {
                         <p className="text-gray-400 mb-6">Convert your PDF document into an interactive mind map.</p>
                         <label className="cursor-pointer  text-black border px-6 py-3 rounded-lg transition-colors inline-flex items-center gap-2">
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-upload" aria-hidden="true"><path d="M12 3v12"></path><path d="m17 8-5-5-5 5"></path><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
-                            </svg>Choose PDF File<input accept=".pdf" className="hidden" type="file" />
+                            </svg>Choose PDF File<input accept=".pdf" className="hidden" type="file" onChange={handleFileUpload} />
                         </label>
                     </div>
                 </div>
