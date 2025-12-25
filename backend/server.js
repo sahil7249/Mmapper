81ff9c811b108f63a4db40874a937417a19f9336
sahil7249 <pathansahil7249@gmail.com>
2025-12-25 18:42:28 +0530

    add cors policy

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,22 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 import processRouter from './router/process.router.js'
 
 dotenv.config({path:'.env'})
 
 const app = express()
 const PORT = process.env.PORT || 8000
+const corsOptions = {
+    origin:'http://localhost:5173',
+    optionsSuccessStatus:200
+}
+
 
 app.use(express.json())
+app.use(cors(corsOptions))
 
-app.use('/api',upload.single('pdf'),processRouter)
+app.use('/api',processRouter)
 
 app.listen(PORT,() => {
     console.log(`App is listening on http://localhost:${PORT}`)
