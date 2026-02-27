448c390d2fd7e5aa426657e9fa9792f2e194ef28
sahil7249 <pathansahil7249@gmail.com>
2026-02-28 02:57:28 +0530

    add update,delete and get MapById

--- backend/controller/map.controller.js
+++ backend/controller/map.controller.js
@@ -1,7 +1,7 @@
 import { Map } from "../database/map.model.js"
 
 const saveMindMapToDB = async (req, res) => {
-    const { title,markdown_content} = req?.body
+    const { title, markdown_content } = req?.body
 
     if ([title, markdown_content].some((field) => field.trim() === "")) {
         return res.status(400).json({
@@ -29,18 +29,85 @@ const saveMindMapToDB = async (req, res) => {
 }
 
 
-const getAllMapData = async (req,res) => {
+const getAllMapData = async (req, res) => {
     const mapData = await Map.find()
 
-    if(!mapData){
+    if (!mapData) {
         return res.status(404).json({
-            message:"Map data is empty"
+            message: "Map data is empty"
         })
     }
 
     return res.json({
-        data:mapData
+        data: mapData
     })
 }
 
-export { saveMindMapToDB,getAllMapData }
+const getMapById = async (req, res) => {
+    const { id } = req?.params
+    try {
+        const mapData = await Map.findById(id)
+
+        if (!mapData) {
+            return res.status(404).json({
+                message: "Map data not found"
+            })
+        }
+
+        console.log(`Data extracted for id : ${id}`)
+        
+
+        return res.json({
+            data: mapData
+        })
+    } catch (error) {
+        console.log('Error: ', error.message)
+    }
+}
+
+const updateMapById = async (req, res) => {
+    const { id } = req?.params
+    const { updateData } = req?.body
+
+    try {
+        const result = await Map.findByIdAndUpdate(id,{markdown_content:updateData},{new:true})
+
+        if (!result) {
+            return res.status(404).json({
+                message: "Map data not found"
+            })
+        }
+    
+        console.log(`Map data updated id : ${id}`)
+
+        return res.json({
+            message: "Map data is updated"
+        })
+    } catch (error) {
+        console.log('Error: ', error.message)
+    }
+}
+
+const deleteMapById = async (req,res) => {
+    const { id } = req?.params
+    try {   
+        const dbResponse = await Map.findByIdAndDelete(id)
+
+        if(!dbResponse){
+            return res.status(404).json({
+                message:"Map data not found"
+            })
+        }
+
+        console.log(`Map data is deleted for id : ${id}`)
+
+        return res.json({
+            message:"Map data is deleted"
+        })
+
+    } catch (error) {
+        console.log("Error: ",error.message)
+    }
+}
+
+export { saveMindMapToDB, getAllMapData, getMapById,updateMapById,deleteMapById }
