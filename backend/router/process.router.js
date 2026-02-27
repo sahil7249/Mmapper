5f45c88f0ae7c1607b863121a4f5abcb31c09ca9
sahil7249 <pathansahil7249@gmail.com>
2026-02-28 02:58:32 +0530

    add get,delete and update MapById routes

--- backend/router/process.router.js
+++ backend/router/process.router.js
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 import { uploadAndProcessPdf } from '../controller/process.controller.js' 
 import multer from 'multer'
-import { getAllMapData, saveMindMapToDB } from '../controller/map.controller.js'
+import { getAllMapData, saveMindMapToDB,getMapById,updateMapById, deleteMapById } from '../controller/map.controller.js'
 
 const upload = multer({
     dest: 'uploads/'
@@ -13,4 +13,7 @@ const processRouter = Router()
 processRouter.post('/upload-pdf',upload.single('pdf'),uploadAndProcessPdf)
 processRouter.post('/save-map',saveMindMapToDB)
 processRouter.get('/all-maps',getAllMapData)
+processRouter.get('/map/:id',getMapById)
+processRouter.post('/map/:id/update',updateMapById)
+processRouter.delete('/map/:id/delete',deleteMapById)
 export default processRouter
